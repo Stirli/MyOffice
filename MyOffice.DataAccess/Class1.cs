d3aef78843f0a05cd5157b155bec3c54ab478dae
Дмитрий Владимирович Станский <d.v.stanski@outlook.com>
2017-08-25 16:50:14 +0300

    123

---
+++ MyOffice.DataAccess/Class1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GenericModel;
+using MyOffice.Model;
+
+namespace MyOffice.DataAccess
+{
+    public class MyOfficeDataProvider:IMyOfficeDataProdvider
+    {
+        public MyOfficeDataProvider()
+        {
+            new CsvDataProvider(GetSource, )
+        }
+
+        private string GetSource()
+        {
+            return 
+        }
+
+        public IEnumerable<Product> Products { get; }
+    }
+}
